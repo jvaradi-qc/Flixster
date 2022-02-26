8fee48bf9c99887e61929c4c35cb424092861c8c
John Varadi <jvaradi329@gmail.com>
2022-02-25 19:03:29 -0800

    demo_flixterApp.gif added

--- app/src/main/java/com/johnv/flixster/MainActivity.kt
+++ app/src/main/java/com/johnv/flixster/MainActivity.kt
@@ -15,6 +15,7 @@ import org.json.JSONException
 
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key="
 private const val API_KEY = "50e2e2a36e44b96f775a10d2aedb6063"
+private const val PAGE = "&page="
 private const val TAG = "MainActivity"
 
 
@@ -25,13 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     private val movies = mutableListOf<Movie>()
     private lateinit var rvMovies: RecyclerView
-    //private var scrollListener: EndlessRecyclerViewScrollListener? = null
-
-    //override fun onConfigurationChanged(newConfig: Configuration) {
-     //   super.onConfigurationChanged(newConfig)
-    //    orient = getResources().getConfiguration().orientation
-    //    Log.i("MainActivity", "orientVariableinOnConfigChanged: $orient")
-    //}
+    private var scrollListener: EndlessRecyclerViewScrollListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
                 super.onCreate(savedInstanceState)
@@ -41,7 +36,44 @@ class MainActivity : AppCompatActivity() {
                 val movieAdapter = MovieAdapter(this, movies)
                 rvMovies.adapter = movieAdapter
                 rvMovies.layoutManager = LinearLayoutManager(this)
-                //scrollListener = EndlessRecyclerViewScrollListener()
+                scrollListener = object : EndlessRecyclerViewScrollListener(rvMovies.layoutManager as LinearLayoutManager) {
+                    override fun onLoadMore(page: Int, totalItemsCount: Int, view: RecyclerView?) {
+                // Triggered only when new data needs to be appended to the list
+                // Add whatever code is needed to append new items to the bottom of the list
+                    loadNextDataFromApi(page)
+            }
+
+                    private fun loadNextDataFromApi(page: Int) {
+
+                        val client = AsyncHttpClient()
+                        client.get(NOW_PLAYING_URL + API_KEY + PAGE + page, object: JsonHttpResponseHandler() {
+                            override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
+                            ) {
+                                Log.e(TAG, "onFailure: $statusCode")
+                            }
+
+                            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                                //Log.i(TAG, "onSuccess: JSON data $json")
+                                //new code for endless scrolling
+                                //movies.clear() ???
+                                try {
+                                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                                    movieAdapter.notifyDataSetChanged()
+                                    //Log.i(TAG, "Movie List $movies")
+                                } catch (e: JSONException){
+                                    Log.e(TAG, "Encountered exception $e")
+                                }
+                            }
+
+                        })
+                    }
+        }
+
+                // Adds the scroll listener to RecyclerView
+                rvMovies.addOnScrollListener(scrollListener as EndlessRecyclerViewScrollListener)
+
+
                 val client = AsyncHttpClient()
                 client.get(NOW_PLAYING_URL + API_KEY, object: JsonHttpResponseHandler() {
                     override fun onFailure(statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
@@ -52,7 +84,7 @@ class MainActivity : AppCompatActivity() {
                     override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
                         //Log.i(TAG, "onSuccess: JSON data $json")
                         //new code for endless scrolling
-                        //movies.clear()
+                        //movies.clear() ???
                         try {
                             val movieJsonArray = json.jsonObject.getJSONArray("results")
                             movies.addAll(Movie.fromJsonArray(movieJsonArray))
@@ -64,5 +96,6 @@ class MainActivity : AppCompatActivity() {
                     }
 
                 })
+
     }
 }
